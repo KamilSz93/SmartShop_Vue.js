983b6743136408fa323452b25c7af8d6290d40dd
KamilSz93 <kamil.szewczyk9304@gmail.com>
2023-06-24 12:02:41 +0200

    Add top scroll behavior to router

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,9 @@ import Search from "../views/Search.vue";
 import EmptyPage from "../views/EmptyPage.vue";
 
 const router = createRouter({
+  scrollBehavior(to, from, savedPosition) {
+    return { top: 0 }
+  },
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: "/", name: "home", component: Home },
